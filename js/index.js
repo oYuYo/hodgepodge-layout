15b2cfbb55a5a41b48297ae77896a2b712d7c2f3
oYuYo <year0120@gmail.com>
2023-07-17 18:06:03 +0900

    add files

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', (event) => {
+    let buttons = document.querySelectorAll('button')
+    buttons.forEach(btn => {
+        btn.addEventListener('click', event => {
+            let el = document.getElementsByTagName('body')[0];
+            switch (btn.id){
+                case 'btnA':
+                    el.style.setProperty('animation-name', 'bgchange-a');
+                    el.style.setProperty('animation-duration', '1.5s');
+                    break;
+                case 'btnB':
+                    el.style.setProperty('animation-name', 'bgchange-b');
+                    el.style.setProperty('animation-duration', '1.0s');
+                    break;
+                case 'btnC':
+                    el.style.setProperty('animation-name', 'bgchange-c');
+                    el.style.setProperty('animation-duration', '5.0s');
+                    break;
+                case 'btnD':
+                    el.style.setProperty('animation-name', 'bgchange-d');
+                    el.style.setProperty('animation-duration', '0.5s');
+                    break;
+                }
+        });
+    });
+
+});
